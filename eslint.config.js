648df9e171380d0a761465f7019b4eaaddba669a
Fayis <fayfayis74@gmail.com>
2026-07-02 13:33:18 +0530

    feat: added precommit & prepush scripts

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+const tseslint = require('@typescript-eslint/eslint-plugin');
+const tsparser = require('@typescript-eslint/parser');
+
+module.exports = [
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {},
+  },
+];
