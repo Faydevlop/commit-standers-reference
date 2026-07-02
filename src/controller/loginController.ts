6389ce9319fb570f89b0a72660d8435ad4bd4190
Fayis <fayfayis74@gmail.com>
2026-07-02 17:20:52 +0530

    fix: updated response

--- src/controller/loginController.ts
+++ src/controller/loginController.ts
@@ -1,9 +1,5 @@
 import { Request, Response } from 'express';
 
-interface test {
-  data: any;
-}
-
 export const showLoginPage = (_req: Request, res: Response): void => {
   res.status(200).json({
     success: false,
